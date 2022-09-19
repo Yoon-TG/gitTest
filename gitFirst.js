8c5d5e1e068321f93afffc8b7ed301a4cca57644
Yoon-TG <witteny24@gmail.com>
2022-09-19 10:29:07 +0900

    console창 추가2

--- gitFirst.js
+++ gitFirst.js
@@ -2,3 +2,5 @@ let first=1;
 let et = 2;
 console.log('??')
 console.log('again')
+//파일 내에 수정사항이 있을 경우, 저장->변경사항을 스테이징해줌(+기호를 통해).. 그 후에 메시지 작성해서 commit 을 남기면 됨<이 부분까지 수정된 파일이 기록으로 남음
+console.log('배고파요') 
